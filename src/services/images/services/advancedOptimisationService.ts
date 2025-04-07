fb11b492a0de882dc37725779c72df6a727cc338
yash1900 <malhotrayash1900@gmail.com>
2025-04-08 02:07:43 +0530

    Update advancedOptimisationService.ts

--- src/services/images/services/advancedOptimisationService.ts
+++ src/services/images/services/advancedOptimisationService.ts
@@ -1,5 +1,5 @@
 import { supabase } from "@/integrations/supabase/client";
-import { urlCache } from "../cacheService";
+import { urlCache } from "../utils/urlCache";
 import { generateContentHash } from "../utils/hashUtils";
 
 interface OptimizedImage {
