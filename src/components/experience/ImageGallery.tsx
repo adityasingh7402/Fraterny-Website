743e9f2e0d7117434c66d055fdb796debeeec173
yash1900 <malhotrayash1900@gmail.com>
2025-04-10 11:14:10 +0530

    Update ImageGallery.tsx

--- src/components/experience/ImageGallery.tsx
+++ src/components/experience/ImageGallery.tsx
@@ -87,7 +87,7 @@ const ImageGallery = () => {
               width={image.width}
               height={image.height}
               sizes="(max-width: 640px) 50vw, (max-width: 1024px) 33vw, 33vw"
-              objectFit="cover"
+              objectFit="contain"
             />
           </div>
         ))}
