cc000e936ca0a608f53ab3dc0ab7f42a6976c2e7
Indranil Maiti <indranilmaiti1@gmail.com>
2025-07-25 19:58:47 +0200

    buttons

--- src/components/quest/layout/QuestNavigation.tsx
+++ src/components/quest/layout/QuestNavigation.tsx
@@ -266,7 +266,7 @@ const handleNext = () => {
             animate="animate"
             whileTap="tap"
             onClick={handleNext}
-            className={`px-4 py-2 rounded-lg text-white text-sm font-medium transition-colors ${
+            className={`px-4 py-2 text-white text-sm font-medium transition-colors ${
               isLastQuestion && showFinish
                 ? 'w-4/5 h-14 bg-gradient-to-br from-sky-800 to-sky-400 rounded-[36px] border-2 border-blue-950'
                 : 'w-full h-14 bg-gradient-to-br from-sky-800 to-sky-400 rounded-[36px] border-2 border-blue-950'
