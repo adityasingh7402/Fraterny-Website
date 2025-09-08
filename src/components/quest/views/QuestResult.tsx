ec53a6d3ec69565744ef7fe0f369345a449804aa
Indranil Maiti <indranilmaiti1@gmail.com>
2025-09-08 07:47:36 +0200

    fix-2

--- src/components/quest/views/QuestResult.tsx
+++ src/components/quest/views/QuestResult.tsx
@@ -1767,10 +1767,20 @@ const [selectedFindingIndex, setSelectedFindingIndex] = useState<number>(0);
 
   if (!resultData) {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="text-center">
-        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600 mx-auto mb-4"></div>
-        <p className="text-gray-600">Loading your results...</p>
+    // <div className="min-h-screen flex items-center justify-center">
+    //   <div className="text-center">
+    //     <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600 mx-auto mb-4"></div>
+    //     <p className="text-gray-600">Loading your results...</p>
+    //   </div>
+    // </div>
+    <div className='h-screen bg-[#004A7F] max-h-screen relative overflow-hidden flex items-center justify-center'>
+      <div className="text-center px-4">
+        <h2 className="text-4xl font-['Gilroy-Bold'] text-white mb-4">
+          Loading your results...
+        </h2>
+        <div className="text-center">
+          <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-white mx-auto mb-4"></div>
+        </div>
       </div>
     </div>
   );
